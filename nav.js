f4d835de47b4eb55ec182002355433e3a348ffd3
Ebuka Odini <ebukaodini@gmail.com>
2020-11-26 00:13:46 +0100

    added localstorage as the major state handler

--- nav.js
+++ nav.js
@@ -1,17 +1,40 @@
 const nav = {
 
    __routes: [],
+   __key: null,
+   global: {},
+   state: {},
 
    start: function() {
       window.addEventListener('DOMContentLoaded', function() {
          window.onpopstate = function(event) {
-            nav.push(document.location.pathname, event.state);
+            nav.push(document.location.pathname, event.state, false);
+         }
+
+         window.onreadystatechange = function(event) {
+            alert('Ready state');
          }
    
          nav.push(window.location.pathname, window.history.state);
       });
    },
    
+   setState: function(params) {
+      // add/update a property in state
+      nav.state = Object.assign(nav.state ?? {}, params ?? {});
+      // update the states in the local storage
+      localStorage.setItem(nav.__key.toString(), JSON.stringify(nav.state));
+      console.log(localStorage);
+   },
+
+   setGlobal: function(params) {
+      // add/update a property in the global state
+      nav.global = Object.assign(nav.global, params ?? {});
+      // update the states in the local storage
+      localStorage.setItem('global', JSON.stringify(nav.global));
+      console.log(localStorage);
+   },
+
    route: function(route = '/', title = 'Document', ...callbacks) {
       nav.__routes.push({
          route: route,
@@ -20,17 +43,25 @@ const nav = {
       });
    },
 
-   push: function(route, params) {
-      console.log(window.history.state)
+   push: function(route, params, addToHistory = true) {
+      // find the route
       for (const key in nav.__routes) {
          if (nav.__routes.hasOwnProperty(key)) {
             const item = nav.__routes[key];
             if (item.route == route) {
+               // add to windows history
+               if (addToHistory) window.history.pushState(params, item.title, item.route);
+               // set the document title
                document.title = item.title;
-               window.history.state = Object.assign(window.history.state ?? {}, params ?? {});
-               window.history.pushState(window.history.state, item.title, item.route);
+               // set the route key
+               nav.__key = key;
+               // set the state from localStorage
+               nav.state = JSON.parse(localStorage.getItem(key.toString()));
+               Object.assign(nav.state ?? {}, params ?? {});
+               // console.log(nav.state);
+               // handle route callbacks
                item.callbacks.forEach(callback => {
-                  callback(window.history.state);
+                  callback(params);
                });
                break;
             }
@@ -39,14 +70,12 @@ const nav = {
    },
 
    replace: function(route, params) {
-      console.log(window.history.state)
       for (const key in nav.__routes) {
          if (nav.__routes.hasOwnProperty(key)) {
             const item = nav.__routes[key];
             if (item.route == route) {
                document.title = item.title;
-               window.history.state = Object.assign(window.history.state ?? {}, params ?? {});
-               window.history.replaceState(window.history.state, item.title, item.route);
+               window.history.replaceState(params, item.title, item.route);
                item.callbacks.forEach(callback => {
                   callback(params);
                });
@@ -57,17 +86,17 @@ const nav = {
    },
 
    forward: function() {
-      console.log(window.history.state)
+      // console.log(nav.state)
       window.history.forward();
    },
 
    back: function() {
-      console.log(window.history.state)
+      // console.log(nav.state)
       window.history.back();
    },
    
    pop: function() {
-      console.log(window.history.state)
+      // console.log(nav.state)
       window.history.back();
    },
 
